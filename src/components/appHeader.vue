f5c32b8d801615e5bf554069f568d9f298d217a0
Daniel Radia <danielradia21@gmail.com>
2021-12-01 21:34:02 +0200

    Beny is a ah yakar

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="main-header-container">
     <div class="main-header-left">
-      <div class="logo">
+      <div class="logo main-header-tabs">
         <router-link to="/workspace">DigiTask</router-link>
       </div>
-      <div class="main-header-workspace" @click="setType('workSpace')">
+      <div class="main-header-tabs" @click="setType('workSpace')">
         Workspace
         <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
       </div>
-      <div class="main-header-recent" @click="setType('recent')">
+      <div class="main-header-tabs" @click="setType('recent')">
         Recent
         <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
       </div>
-      <div class="main-header-starred" @click="setType('starred')">
+      <div class="main-header-tabs" @click="setType('starred')">
         Starred
         <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
       </div>
-      <div class="main-header-create">
+      <div class="main-header-tabs">
         Create
         <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
       </div>
@@ -26,8 +26,8 @@
         <input type="text" placeholder="Search" class="main-header-search" />
         <span class="search"><i class="fas fa-search"></i></span>
       </div>
-      <div class="main-header-bell">bell</div>
-      <div class="main-header-profile" @click="setType('profile')">PROFILE</div>
+      <div class="main-header-bell"><i class="far fa-bell"></i></div>
+      <div class="main-header-profile" @click="setType('profile')">DR</div>
     </div>
     <header-dynamic class="abs" :type="type" v-if="type"></header-dynamic>
   </section>
