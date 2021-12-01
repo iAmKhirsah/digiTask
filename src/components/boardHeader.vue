706639770ec3a89763f45abf28b8c779e7f7fe4c
Beny.K <Beku1997@gmail.com>
2021-12-01 20:05:46 +0200

    Push for daniel

--- src/components/boardHeader.vue
+++ src/components/boardHeader.vue
@@ -1,4 +1,10 @@
 <template>
+  <header class="border-header">
+        <div>
+          <h1 class="board-box">{{ board.title }}</h1>
+        </div>
+        <p>board background/img goes here</p>
+      </header>
 </template>
 <script>
 export default {
