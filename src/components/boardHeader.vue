8939f3ff4513a6520b6ee43e2205ee070b18bfb8
Daniel Radia <danielradia21@gmail.com>
2021-12-02 01:15:35 +0200

    alla wacbar !

--- src/components/boardHeader.vue
+++ src/components/boardHeader.vue
@@ -1,22 +1,33 @@
 <template>
-  <header class="border-header">
-        <div>
-          <h1 class="board-box">{{ board.title }}</h1>
-        </div>
-        <p>board background/img goes here</p>
-      </header>
+  <header class="board-header">
+    <div class="board-box">
+      <input :style="inputWidth" v-model="board.title" />
+    </div>
+    <div class="board-box star" @click="toggleStar">
+      <span v-if="!isStarred"><i class="far fa-star"></i></span>
+      <span v-else><i class="fas fa-star"></i></span>
+    </div>
+  </header>
 </template>
 <script>
 export default {
   name: "boardHeader",
-  props:['board'],
-  data(){
-      return{
-
-      }
+  props: ["board"],
+  data() {
+    return {
+      isStarred: false,
+    };
   },
-  created(){
-
+  created() {},
+  computed: {
+    inputWidth() {
+      return { width: this.board.title.length * 10 + 24 + "px" };
+    },
   },
+  methods:{
+    toggleStar(){
+      this.isStarred = !this.isStarred
+    }
+  }
 };
 </script>
