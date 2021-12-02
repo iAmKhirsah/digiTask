7843b0e329a5f62d43acac41ae424e1e560e93e6
Beny.K <Beku1997@gmail.com>
2021-12-02 12:31:22 +0200

    changes

---
+++ src/components/dynamic_components/invite.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="dynamic-invite card-layout nav-modal">
+    <div class="header-layout">
+      <header>Invites</header>
+    </div>
+    <div>
+      <p class="subtitle">Current Workspace</p>
+    </div>
+    <div class="workspace-contant">
+      <div class="name-tag">I</div>
+      <p>Ilya Shershniov's workspace</p>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "recent",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async created(){
+      await this.$store.dipatch({type:'loadUsers'})
+      this.users = this.$store.getters.userss
+  }
+};
+</script>
