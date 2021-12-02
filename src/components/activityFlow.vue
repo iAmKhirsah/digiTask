cc48721031c1b8d6a2fe0a6bb13643c35a6ee8ef
Ilya <khirsah2211@gmail.com>
2021-12-03 01:22:09 +0200

    activity flow work started

---
+++ src/components/activityFlow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- <div>{{ getActivities }}</div> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "activityFlow",
+  props: ["task", "group", "board"],
+  computed: {
+    getActivities() {
+      console.log(this.board);
+      let idx = this.board.activities.findIndex(
+        (activity) => activity.task.id === this.task.id
+      );
+      return this.board.activities[idx];
+    },
+    getComments() {},
+  },
+};
+</script>
