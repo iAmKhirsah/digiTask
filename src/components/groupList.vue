5b1473c5b5337e81b55048a0fa9f70df92f6b78a
Daniel Radia <danielradia21@gmail.com>
2021-12-02 20:45:30 +0200

    jfhdsk

--- src/components/groupList.vue
+++ src/components/groupList.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="group-list-container">
     <div v-for="group in boardGroups" :key="group.id" class="group-list-group">
-<<<<<<< HEAD
-     <!-- beny -->
-      <div class="group-header">
-        <p>{{ group.title }}</p>
-=======
       <div>
         <textarea v-model="group.title" class="group-title" @change="updateGroup"/>
         <!-- <span class="input" role="textbox"  contenteditable @change="updateGroup">{{group.title}}</span> -->
->>>>>>> 283302f8ca538f12e86d77ce450f013d3a797bbf
         <button>edit</button>
       </div>
       <task-list :group="group" @editTask="editTask" @addTask="addTask" />
