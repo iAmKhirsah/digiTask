17592151498f0fc4c41df27c98d7d9380df729f9
Daniel Radia <danielradia21@gmail.com>
2021-12-02 17:56:51 +0200

    walllaaaa

--- src/components/dynamic_components/workSpace.vue
+++ src/components/dynamic_components/workSpace.vue
@@ -12,7 +12,7 @@
       <p>Ilya Shershniov's workspace</p>
     </div>
     <div class="card-line"></div>
-    <div class="card-main-line"></div>
+   
     <div>
       <p class="subtitle">Your Workspaces</p>
     </div>
@@ -21,7 +21,7 @@
       <p>Digitask workspace</p>
     </div>
 
-    <div class="card-main-line"></div>
+ 
     <div>
       <p class="subtitle">Guest Workspaces</p>
     </div>
@@ -39,15 +39,11 @@ export default {
       boards: null,
     };
   },
-<<<<<<< HEAD
   methods: {
     closeModal() {
       console.log("hello");
       this.$emit("closeModal");
     },
   },
-=======
-  methods: {},
->>>>>>> 0c32171cca82cfe1edc6c6cc4994af2afabbf852
 };
 </script>
