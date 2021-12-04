fe35a35fde982459a2ae638e34bf62be68120ad5
Ilya <khirsah2211@gmail.com>
2021-12-04 16:28:27 +0200

    beny stop

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,37 +1,49 @@
 <template>
-  <section class="main-header-container" v-click-outside="setType">
-    <div class="main-header-left">
-      <span class="trello-logo"><i class="fab fa-trello"></i></span>
-      <div class="logo main-header-tabs">
-        <router-link to="/workspace">DigiTask</router-link>
+  <div>
+    <section class="main-header-container" v-click-outside="setType">
+      <div class="main-header-left">
+        <span class="trello-logo"><i class="fab fa-trello"></i></span>
+        <div class="logo main-header-tabs">
+          <router-link to="/workspace">DigiTask</router-link>
+        </div>
+        <div class="main-header-tabs" @click="setType('workSpace')">
+          Workspace
+          <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
+        </div>
+        <div class="main-header-tabs" @click="setType('recent')">
+          Recent
+          <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
+        </div>
+        <div class="main-header-tabs" @click="setType('starred')">
+          Starred
+          <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
+        </div>
+        <div class="main-header-tabs">
+          Create
+          <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
+        </div>
       </div>
-      <div class="main-header-tabs" @click="setType('workSpace')">
-        Workspace
-        <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
+      <div class="main-header-right">
+        <div class="input-container">
+          <input type="text" placeholder="Search" class="main-header-search" />
+          <span class="search"><i class="fas fa-search"></i></span>
+        </div>
+        <div class="main-header-bell"><i class="far fa-bell"></i></div>
+        <div
+          class="main-header-profile user-tag-name in-header"
+          @click="setType('profile')"
+        >
+          DR
+        </div>
       </div>
-      <div class="main-header-tabs" @click="setType('recent')">
-        Recent
-        <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
-      </div>
-      <div class="main-header-tabs" @click="setType('starred')">
-        Starred
-        <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
-      </div>
-      <div class="main-header-tabs">
-        Create
-        <span class="arrow-down"><i class="fas fa-chevron-down"></i></span>
-      </div>
-    </div>
-    <div class="main-header-right">
-      <div class="input-container">
-        <input type="text" placeholder="Search" class="main-header-search" />
-        <span class="search"><i class="fas fa-search"></i></span>
-      </div>
-      <div class="main-header-bell"><i class="far fa-bell"></i></div>
-      <div class="main-header-profile user-tag-name in-header" @click="setType('profile')">DR</div>
-    </div>
-    <header-dynamic  @closeModal="closeModal" class="abs" :type="type" v-if="type"></header-dynamic>
-  </section>
+    </section>
+    <header-dynamic
+      @closeModal="closeModal"
+      class="abs"
+      :type="type"
+      v-if="type"
+    ></header-dynamic>
+  </div>
 </template>
 <script>
 import headerDynamic from "./headerDynamic.vue";
@@ -50,9 +62,9 @@ export default {
     setType(type) {
       this.type = type;
     },
-    closeModal(){
-       this.setType('')
-    }
+    closeModal() {
+      this.setType("");
+    },
   },
   directives: {
     clickOutside: vClickOutside.directive,
