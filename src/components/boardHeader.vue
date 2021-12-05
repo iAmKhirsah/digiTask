3df50d630927c60139eb9f04cba3e185f5daf882
Ilya <khirsah2211@gmail.com>
2021-12-05 11:42:11 +0200

    69.99%

--- src/components/boardHeader.vue
+++ src/components/boardHeader.vue
@@ -1,24 +1,34 @@
 
 <template>
-  <header class="board-header" v-click-outside="setType">
-    <div class="board-header-left">
-      <div class="board-box">
-        <input :style="inputWidth" v-model="board.title" @focus="$event.target.select()" />
-      </div>
-      <div class="board-box star" @click="toggleStar">
-        <span v-if="!isStarred"><i class="far fa-star"></i></span>
-        <span v-else><i class="fas fa-star"></i></span>
-      </div>
-      <div>
-        <div v-for="(user, idx) in board.memebers" :key="idx" class="members">
-          <span class="member"></span>
+  <section>
+    <header class="board-header" v-click-outside="setType">
+      <div class="board-header-left">
+        <div class="board-box">
+          <input
+            :style="inputWidth"
+            v-model="board.title"
+            @focus="$event.target.select()"
+          />
         </div>
-        <div class="board-box invite" @click="setType('invite')">Invite</div>
+        <div class="board-box star" @click="toggleStar">
+          <span v-if="!isStarred"><i class="far fa-star"></i></span>
+          <span v-else><i class="fas fa-star"></i></span>
+        </div>
+        <div>
+          <div v-for="(user, idx) in board.memebers" :key="idx" class="members">
+            <span class="member"></span>
+          </div>
+          <div class="board-box invite" @click="setType('invite')">Invite</div>
+        </div>
       </div>
-      <header-dynamic @closeModal="closeModal" :type="type" v-if="type"></header-dynamic>
-    </div>
-    <div class="board-header-right"></div>
-  </header>
+      <div class="board-header-right"></div>
+    </header>
+    <header-dynamic
+      @closeModal="closeModal"
+      :type="type"
+      v-if="type"
+    ></header-dynamic>
+  </section>
 </template>
 
 
@@ -50,7 +60,7 @@ export default {
       this.type = type;
     },
     closeModal() {
-      this.setType('');
+      this.setType("");
     },
   },
   components: {
