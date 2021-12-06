66297b9498eed90c7f95fdf193b7d8c1d93ff1e5
Daniel Radia <danielradia21@gmail.com>
2021-12-06 04:00:18 +0200

    goo djob

--- src/components/dynamic_components/group-edit.vue
+++ src/components/dynamic_components/group-edit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dynamic-group-edit card-layout nav-modal">
+  <div class="dynamic-group-edit">
     <button @click="closeModal"> <span class="material-icons"> clear </span></button>
       <div class="header-layout">
       <header>List ations</header>
--- src/components/taskPreviewLabel.vue
+++ src/components/taskPreviewLabel.vue
@@ -5,28 +5,31 @@
       :key="label.id"
       class="task-labels-container"
       @click.capture="miniPreview"
-      
     >
-    <!-- <transition name="fade" mode="out-in"> -->
-    <div    class="label-preview" :class="labelMini"  :style="{background:label.color}"><span class="label-preview" v-if="!isMiniPreview">{{label.title}}</span></div>
+      <!-- <transition name="fade" mode="out-in"> -->
+      <div
+        class="label-preview"
+        :class="labelMini"
+        :style="{ background: label.color }"
+      >
+      <transition name="fade" >  <span class="label-preview" v-if="!isMiniPreview"> <span class="label-title">{{ label.title }} </span> </span></transition> 
+      </div>
       <!-- <div   class="label-preview" v-else :style="{ background: label.color , ':hover-opacity':0.3}">{{label.title}}</div> -->
-     <!-- </transition> -->
+      <!-- </transition> -->
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "taskLabelPreview",
-  props: ["task", "board","isMiniPreview"],
-  data(){
-      return {
-    
-      }
+  props: ["task", "board", "isMiniPreview"],
+  data() {
+    return {};
   },
   methods: {
-      miniPreview(){
-          this.$emit("miniPreview")
-      },
+    miniPreview() {
+      this.$emit("miniPreview");
+    },
     editTask(taskId) {
       this.$emit("editTask", taskId);
     },
@@ -45,8 +48,8 @@ export default {
       console.log(labels);
       return labels;
     },
-    labelMini(){
-        return {'mini' : this.isMiniPreview}
+    labelMini() {
+      return { mini: this.isMiniPreview };
     },
     backgroundColor() {},
   },
