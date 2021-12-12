cd699430dead4d70a5d8cebe579f21e173476957
Ilya <khirsah2211@gmail.com>
2021-12-12 03:00:45 +0200

    ?

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pwa: {
     name: 'digiTask',
-    workboxPluginMode: 'GenerateSW',
+    workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: 'src/service-worker.js',
     },
